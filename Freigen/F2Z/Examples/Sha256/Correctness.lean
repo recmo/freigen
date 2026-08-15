9cdde86a474b96be92fac8daf34d1a71f4fa92b7
Remco Bloemen <remco@wicked.ventures>
2026-08-15 22:12:02 +0200

    add weighted circuit cost to stats

--- Freigen/F2Z/Examples/Sha256/Correctness.lean
+++ Freigen/F2Z/Examples/Sha256/Correctness.lean
@@ -53,7 +53,7 @@ example : perm
     ] := by native_decide
 
 /--
-info: { mRows := 20457, mCols := 7145, r1csRows := 184 }
+info: { mRows := 20457, mCols := 7145, r1csRows := 184, cost := 150344 }
 -/
 #guard_msgs in
 #eval sha256CS.2.stats
